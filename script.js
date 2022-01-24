5ad44e6a4142f0821e9de7612f970849df55365f
Adrian Croitoru <adrian.croitoru.97@gmail.com>
2022-01-24 19:06:13 +0200

    added delete functionality, working on check

--- script.js
+++ script.js
@@ -2,39 +2,58 @@ let todos = []
 let elementsContainer = document.getElementById('elementsContainer')
 
 function deleteElement(i) {
-    todos.splice(i , i + 2);
+    todos.splice(i, 1);
+    render();
+}
+
+function checkElement(ind) {
+    console.log(todos);
+    console.log(document.getElementById("checkbox" + ind));
+    document.getElementById("checkbox" + ind).click();
+    console.log(document.getElementById("checkbox" + ind));
+    // e.classList.add('round label:after');
+    render();
 }
 
 const render = () => {
     // clean elementsContainer
     elementsContainer.innerHTML =''
 
-    todos.forEach(element => {
-        // console.log(element)
+    todos.forEach((element, ind) => {
         let list_element = document.createElement('div')
         list_element.classList.add('list_element')
 
+
         let checkbox = document.createElement('div')
         checkbox.classList.add('round')
-        checkbox.innerHTML = `<input type="checkbox" unchecked id="checkbox" />
-        <label for="checkbox"></label>`
+        checkbox.addEventListener('click', function() {
+            if (element.checked == true)
+                element.checked = false;
+            else
+                element.checked = true;
+            checkElement(ind);
+        });
+        checkbox.innerHTML = `<input type="checkbox" unchecked id="checkbox` + ind + `" />
+        <label for="checkbox` + ind + `"></label>`
         
+
         let element_of_list = document.createElement('div')
         element_of_list.classList.add('element_of_list')
         element_of_list.innerHTML = element["text"]
         
+
         let button = document.createElement('div')
         button.classList.add('xButton')
-        // button.addEventListener('click', deleteElement);
         button.addEventListener('click', function() {
-            deleteElement(element.id);
+            deleteElement(ind);
         });
         button.innerHTML = `<i class="fa fa-times fa-2x" aria-hidden="true"></i>`
 
+
         list_element.appendChild(checkbox)
-        
         list_element.appendChild(element_of_list)
         list_element.appendChild(button)
+
 
         elementsContainer.appendChild(list_element)
     });
@@ -63,11 +82,16 @@ const main = () => {
         }
     });
 
-    var allButtons = document.getElementsByClassName("xButton");
-    console.log(allButtons);
-    for (var i = 0; i < allButtons.length; i++) {
-        allButtons[i].outerHTML.addEventListener('click', render)
-    }
+    // var allButtons = document.getElementsByClassName("xButton");
+    // for (var i = 0; i < allButtons.length; i++) {
+    //     allButtons[i].outerHTML.addEventListener('click', render);
+    // }
+
+    // var allChecks = document.getElementsByClassName("chck");
+    // console.log(allChecks);
+    // for (var i = 0; i < allChecks.length; i++) {
+    //     allChecks[i].outerHTML.addEventListener('click', render);
+    // }
 }
 
 main()
